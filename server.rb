358330218f654cdec6bae859471b1a4f57f40764
daicyan <daisuke@taruki.com>
2017-02-05 21:57:14 +0900

    スペル修正およびOnset!のroll.rb互換コマンドを追加

--- server.rb
+++ server.rb
@@ -13,7 +13,7 @@ end
 
 
 get "/" do
-  "Hello. This is DCDice-API."
+  "Hello. This is BCDice-API."
 end
 
 get "/v1/version" do
@@ -29,16 +29,41 @@ get "/v1/diceroll" do
   if dicebot.nil? || params[:command].nil?
     return json ok: false
   end
-
+  
   bcdice = BCDiceMaker.new.newBcDice
   bcdice.setDiceBot(dicebot)
   bcdice.setMessage(params[:command])
-
+  
   result, secret = bcdice.dice_command
-
+  
   if result.nil?
     json ok: false
   else
     json ok: true, result: result, secret: secret
   end
 end
+
+get "/v1/OnsetCompat" do
+  result = ""
+
+  if params[:list] == "1"
+    BCDice::SYSTEMS.sort.each do |game|
+      result += game + "\n"
+    end
+    result
+  elsif params[:text]
+    bcdice = BCDiceMaker.new.newBcDice
+    bcdice.setDiceBot(BCDice::DICEBOTS[params[:system]])
+    bcdice.setMessage(params[:text])
+    
+    result, secret = bcdice.dice_command
+    if result.nil?
+      "error"
+    else
+      "onset" + result
+    end
+  else
+    "error"
+  end
+end
+
