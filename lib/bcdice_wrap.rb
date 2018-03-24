d1604cceab37f24e895313010bb935234de83de7
SAKATA Sinji <sinji@sakasin.net>
2018-03-24 20:11:14 +0900

    Update BCDice to Ver2.02.76

--- lib/bcdice_wrap.rb
+++ lib/bcdice_wrap.rb
@@ -4,7 +4,7 @@ require "diceBot/DiceBot"
 require "diceBot/DiceBotLoader"
 
 class BCDice
-  VERSION = "2.02.72"
+  VERSION = "2.02.76"
 
   DICEBOTS = (DiceBotLoader.collectDiceBots + [DiceBot.new]).
     map { |diceBot| [diceBot.gameType, diceBot] }.
