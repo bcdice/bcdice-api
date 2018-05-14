598cb5a579d9e958b052a06a2f605cd277eb7f6d
SAKATA Sinji <sinji@sakasin.net>
2018-05-14 22:30:17 +0900

    Actually enable CORS

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ source "https://rubygems.org"
 gem "sinatra"
 gem "sinatra-contrib"
 gem "sinatra-jsonp"
-gem "sinatra-cross_origin", "~> 0.4.0"
 
 gem "puma"
 
--- server.rb
+++ server.rb
@@ -5,16 +5,11 @@ $:.unshift File.join(__dir__, "lib")
 
 require 'sinatra'
 require 'sinatra/jsonp'
-require 'sinatra/cross_origin'
 require 'bcdice_wrap'
 require 'exception'
 
 module BCDiceAPI
   VERSION = "0.5.3"
-end
-
-configure do
-  enable :cross_origin
 end
 
 configure :production do
@@ -46,6 +41,10 @@ helpers do
 
     return result, secret, dices
   end
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
 end
 
 get "/" do
