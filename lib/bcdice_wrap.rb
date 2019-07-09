503511d05a09fc7d61a608f75ddac879dda1a579
SAKATA Sinji <sinji@sakasin.net>
2019-07-09 22:12:20 +0900

    BCDice::VERSION is supported by official

--- lib/bcdice_wrap.rb
+++ lib/bcdice_wrap.rb
@@ -4,8 +4,6 @@ require "diceBot/DiceBot"
 require "diceBot/DiceBotLoader"
 
 class BCDice
-  VERSION = "2.02.80"
-
   DICEBOTS = (DiceBotLoader.collectDiceBots + [DiceBot.new]).
     map { |diceBot| [diceBot.gameType, diceBot] }.
     sort.
